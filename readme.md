7f47996a1342dc5e20c161be31d3bc133fc83896
Raluca Nicola <ralucagnicola@gmail.com>
2024-01-14 15:51:53 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Shaders
+
+## Setup
+
+Download [Node.js](https://nodejs.org/en/download/).
+Run this followed commands:
+
+```bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server
+npm run start
+
+# Build for production in the dist/ directory
+npm run build
+```
---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,21 @@
+varying vec2 vuv;
+
+#define PI 3.14159265
+
+float random(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+    
+
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
